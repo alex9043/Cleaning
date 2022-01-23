0d531857c92928e95566446b7a1af7d684d0bfb3
alex9034 <alex980127@gmail.com>
2022-01-23 18:28:13 +0300

    Commit

--- order/views.py
+++ order/views.py
@@ -2,7 +2,7 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
 from .form import OrderForm
-from .models import Order, User
+from .models import CleaningType, Order, User, Service, ServicesInOrder, Status
 from datetime import datetime
 
 mothsDict = {"Январь": 1, "Февраль": 2, "Март": 3, "Апрель": 4, "Май": 5, "Июнь": 6, "Июль": 7, "Август": 8, "Сентябрь": 9, "Октябрь": 10, "Ноябрь": 11, "Декабрь": 12}
@@ -22,21 +22,18 @@ def order(request):
         phone = request.POST.get("phone")
         email = request.POST.get("email")
         address = request.POST.get("address")
-        order = Order()
-        for e in User.objects.all():
-            if e.firstName == firstName and e.secondName == secondName and e.phone == phone and e.email == email:
-                user = e
-            else:
-                user = User.objects.create(firstName=firstName, secondName=secondName, phone=phone, email=email)
-        order.User = user
-        order.area = area
-        order.dateTime = "{0}-{1}-{2} {3}".format(datetime.now().year, monthNum, day, time)
+        order = Order(
+            CleaningType = CleaningType.objects.get(name = cleaningType), 
+            area = area, 
+            dateTime = "{0}-{1}-{2} {3}".format(datetime.now().year, monthNum, day, time), 
+            User = User.objects.get(phone = phone),
+            totalPrice = 0, 
+            status = Status.objects.get(name = "Получен"), 
+        )
         order.save()
         # return HttpResponse("<h2>cleaningType = {0}</h2><h2>area = {1}</h2><h2>serviceName = {2}</h2><h2>serviceNumber = {3}</h2><h2>day = {4}</h2><h2>time = {5}</h2><h2>firstName = {6}</h2><h2>secondName = {7}</h2><h2>phone = {8}</h2><h2>email = {9}</h2><h2>address = {10}</h2>".format(cleaningType, area, serviceName, serviceNumber, date, time, firstName, secondName, phone, email, address))
         return HttpResponseRedirect("/order")
     else:
         orderform = OrderForm()
         return render(request, "pages/order_test.html", {"form": orderform})
-    
     # return render(request, "pages/application.html")
-    
